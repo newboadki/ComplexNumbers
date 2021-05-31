f7b0997fd46449a7f3023edbf422302c965580ec
Borja Arias Drake <borja.arias@gmail.com>
2021-05-31 20:35:19 +0200

    Initial Commit

---
+++ Shared/ComplexNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexNumbersApp.swift
+//  Shared
+//
+//  Created by Borja Arias Drake on 31.05.2021..
+//
+
+import SwiftUI
+
+@main
+struct ComplexNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
